a4a0912863b7b8a1c5bd49def91ba220ffd26eec
Lars Nysom <LANY@ucn.dk>
2021-09-15 13:58:10 +0200

    Changed TODO

---
+++ src/DatabaseInvoker.java
@@ -0,0 +1,25 @@
+
+public class DatabaseInvoker {
+
+	// TODO: Exercise 1
+	
+	
+	// TODO: Exercise 2
+	
+	
+	// TODO: Exercise 3
+	
+	
+	// TODO: Exercise 4
+	
+	
+	// TODO: Exercise 5
+	
+	
+	// TODO: Exercise 6
+	
+	
+	// TODO: Exercise 7
+
+	
+}
